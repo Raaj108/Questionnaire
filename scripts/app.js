a6f724eb1cc7fac07526f5695345b77b2840382e
Harshal Vaidya <Harshal Vaidya>
2016-03-31 16:19:08 -0700

    sixth commit, Reset Button

--- scripts/app.js
+++ scripts/app.js
@@ -15,20 +15,33 @@ app.controller('quetionnaireFormCtrl', ['$scope', function ($scope) {
         , domain: ''
         , host: ''
     };
-
+    $scope.phoneNumbr = /^[(]{0,1}[0-9]{3}[)\.\- ]{0,1}[0-9]{3}[\.\- ]{0,1}[0-9]{4}$/;
     $scope.questionnaire = temp;
+    $scope.master = {};
 
-    $("button[type='submit']").click(function () {
-        console.log($scope.questionnaire);
-
-
-    });
 
     $scope.submitForm = function (isValid) {
         if (isValid) {
-            console.log('Hello');
+            console.log($scope.questionnaire);
         }
     }
+
+    /*$('button[name="reset"]').click(function () {
+        if (typeof temp == "object") {
+            console.log('obj');
+            $.each(temp, function (k, v) {
+                // k is either an array index or object key
+                v = "";
+
+            });
+        }
+        $scope.questionnaire = temp;
+    });*/
+
+    $scope.reset = function () {
+        $scope.questionnaire = angular.copy($scope.master);
+    };
+
     console.log($scope.questionnaire);
 
 }]);
